3850b284b56a1491dc28f9edb0fdb1f0e9b58a1d
unknown <naimad2003@wp.pl>
2025-10-08 17:29:58 +0200

    Dodano kontrolery, pokój itp

---
+++ Scripts/XRInit.gd
@@ -0,0 +1,18 @@
+class_name XRInit extends Object
+
+static var xr_interface: XRInterface
+
+static func init(vp: Viewport):
+	if xr_interface != null:
+		return
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		# Change our main viewport to output to the HMD
+		vp.use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
---
+++ Scripts/XRPlayer.gd
@@ -0,0 +1,4 @@
+extends XROrigin3D
+
+func _ready() -> void:
+	XRInit.init(get_viewport())
