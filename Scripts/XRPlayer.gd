0b9601270edde421ee516f70bd501a04214d544d
unknown <naimad2003@wp.pl>
2025-10-10 17:05:54 +0200

    wiatla i poruszanie

--- Scripts/XRPlayer.gd
+++ Scripts/XRPlayer.gd
@@ -2,3 +2,13 @@ extends XROrigin3D
 
 func _ready() -> void:
 	XRInit.init(get_viewport())
+
+'''
+var interface = XRServer.find_interface("name of the plugin")
+	if interface and interface.initalize():
+		get_viewport().arvr = true
+		
+		OS.vsync_enabled = false
+		
+		Engine.iterations_per_second = 90                
+'''
